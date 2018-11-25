47c17713d57a02e057fcbffe7941f58150ad50a7
mika_mccarron <mikamccarron@gmail.com>
2018-11-25 11:28:35 +1100

    sunday noon

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,11 +30,11 @@ q4 = Quotation.create content:'We may encounter many defeats but we must not be 
 
 q5 = Quotation.create content:'give me a <br>', image:'https://images.unsplash.com/photo-1503431128871-cd250803fa41?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=b036ab1a07bbfd47a8d559735b4c40e6&auto=format&fit=crop&w=1050&q=80'
 
-q6 = Quotation.create content:"Your speed doesn't matter,forward is forward.", image:'https://images.unsplash.com/photo-1540999742814-7c7cec92d9b2?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=19f9fa35a5aa2fa1308566f7885b8979&auto=format&fit=crop&w=1051&q=80'
+q6 = Quotation.create content:"Your speed doesn't matter, forward is forward.", image:'https://images.unsplash.com/photo-1540999742814-7c7cec92d9b2?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=19f9fa35a5aa2fa1308566f7885b8979&auto=format&fit=crop&w=1051&q=80'
 
 q7 = Quotation.create content:'Turn your wounds into wisdom', image:'https://images.unsplash.com/photo-1522008660239-1bbdb39444c4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=38434d91d60775352e25e464c658a33b&auto=format&fit=crop&w=1050&q=80'
 
-q8 = Quotation.create content:'Your time is limited, so don’t waste it living someone else’s life.', image:'https://cbsnews2.cbsistatic.com/hub/i/r/2011/10/23/be2ab4a9-a644-11e2-a3f0-029118418759/thumbnail/1200x630/bd2191b23dff664737d3146de750ea49/SteveJobs_Wide.jpg'
+q8 = Quotation.create content:'I want a Pixel 3.', image:'https://cbsnews2.cbsistatic.com/hub/i/r/2011/10/23/be2ab4a9-a644-11e2-a3f0-029118418759/thumbnail/1200x630/bd2191b23dff664737d3146de750ea49/SteveJobs_Wide.jpg'
 
 puts "Created #{Quotation.all.length} quotations"
 puts "Done!"
