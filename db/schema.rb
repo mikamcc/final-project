a65e6980cee6884ba7131352ebfaaea691c2b5f7
mika_mccarron <mikamccarron@gmail.com>
2018-11-29 21:25:22 +1100

    created pg database

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_11_29_012437) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "quotations", force: :cascade do |t|
     t.integer "user_id"
     t.text "content"
